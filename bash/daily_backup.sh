78f1d847a1588f1ff62d2f9a728cecc3be8c8001
itsf4llofstars <irooted4hal@mailfence.com>
2023-04-21 14:42:11 -0500

    Copy bash scripts to repo

---
+++ bash/daily_backup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+TARFILE="$HOME"/archives/daily_backup.tar
+NVIMCONF=""
+KITTYCONF="$HOME"/.config/kitty/kitty.conf
+
+if [ -f "$HOME"/.config/nvim/init.vim ]; then
+  NVIMCONF="$HOME"/.config/nvim/init.vim
+elif [ -f "$HOME"/.config/nvim/init.lua ]; then
+  NVIMCONF="$HOME"/.config/nvim/init.lua
+fi
+
+LOGDIR="$HOME"/logfiles/mint.log
+LOGDATE=$(date +'%FT%T')
+
+if [ -f "$TARFILE" ]; then
+  rm "$TARFILE"
+fi
+
+DOTFILES=(.bashrc .bash_aliases .gitconfig .tmux.conf .nanorc)
+
+tar --create --absolute-names --file="$TARFILE" "$NVIMCONF"
+tar --append --absolute-names --file="$TARFILE" "$KITTYCONF"
+
+for dotfile in "${DOTFILES[@]}"; do
+  tar --append --absolute-names --file="$TARFILE" "$HOME"/"$dotfile"
+done
+
+printf "%s\n" "[$LOGDATE] $0" >> "$LOGDIR" 2>&1
+
+exit 0
---
+++ bash/mod.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+PERS=755
+FILE="$1"
+
+chmod "$PERS" "$FILE"
+
+exit
