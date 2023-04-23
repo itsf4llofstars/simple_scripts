1507204eddbd993078ef5a7c69d84ba2e932f979
itsf4llofstars <irooted4hal@mailfence.com>
2023-04-23 02:16:32 -0500

    Add simple help test to color.sh

--- bash/colors.sh
+++ bash/colors.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/bash
 # shellcheck disable=SC2059
 
+clear
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  printf "%s\n" "    Prints numbers 0-255 in the color that each 256 number"
+  printf "%s\n" "    represents."
+  printf "\n%s\n" "    ~ $ ./colors.sh [options]"
+  printf "\n%s\n" "    OPTIONS"
+  printf "%s\n" "        -h, --help Print this help and exit."
+  printf "%s\n" "        -n Print numbers and colors in a single column"
+  exit
+fi
+
 if [ "$1" == "-n" ]; then
-        for i in {0..255}; do
-                printf "\x1b[38;5;${i}m${i}\n"
-        done
+  for i in {0..255}; do
+    printf "\x1b[38;5;${i}m${i}\n"
+  done
 else
-        for i in {0..255}; do
-                printf "\x1b[38;5;${i}m${i} "
-        done
+  for i in {0..255}; do
+    printf "\x1b[38;5;${i}m${i} "
+  done
 fi
 echo
-
