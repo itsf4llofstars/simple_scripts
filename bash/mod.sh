75664b7a31f20aafaf974363290e05e4297e80f5
itsf4llofstars <irooted4hal@mailfence.com>
2023-05-16 15:33:39 -0500

    Add help documentation

--- bash/mod.sh
+++ bash/mod.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/bash
+# mod.sh
+# Script used to quickly change a files permissions
+# to executable
+
+if [ "$1" == "-h"] || [ "$1" == "--help" ]
+then
+    printf "\t%s\n\n" "Changes a files permission to executable"
+    printf "\t%s\n\n" "\$ ./mod.sh file"
+fi
 
 PERS=755
 FILE="$1"
