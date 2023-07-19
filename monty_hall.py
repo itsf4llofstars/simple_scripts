6dd37dac55b485c923acc272bf507b1f8570b4dc
itsf4llofstars <irooted4hal@mailfence.com>
2023-07-18 19:29:00 -0500

    Change switch to swap in if condition

--- monty_hall.py
+++ monty_hall.py
@@ -26,32 +26,36 @@ for k, v in door_dict.items():
     print(k, v)
 print()
 
-door_choice = input("Enter your door choice 1, 2, 3: ")
+# for testing
+# door_choice = input("Enter your door choice 1, 2, 3: ")
+door_choice = "2"
 
 show_door = ""
 for door in door_dict.keys():
-    if door == door_choice or door_dict[door] == 'gold':
+    if door == door_choice or door_dict[door] == "gold":
         continue
     show_door = door
     print(f"Door: {show_door} is a {door_dict[door]}")
 
 swap: bool = None
 
-switch = input("Do you want to switch [y/n]? ")
+# for testing
+# switch = input("Do you want to switch [y/n]? ")
+switch = "y"
 
-if switch == 'y':
+if switch == "y":
     swap = True
 else:
     swap = False
 
-if switch:
+if swap:
     for door in door_dict.keys():
-        if door_dict[door] == 'gold':
+        if door_dict[door] == "gold":
             win += 1
         else:
             lose += 1
 else:
-    if door_dict[door_choice] == 'gold':
+    if door_dict[door_choice] == "gold":
         win += 1
     else:
         lose += 1
