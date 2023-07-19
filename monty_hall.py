ad16774708cab24a294e8712774d0d3dacd5f8cd
itsf4llofstars <irooted4hal@mailfence.com>
2023-07-18 21:22:44 -0500

    Change to dict teration through items

--- monty_hall.py
+++ monty_hall.py
@@ -20,20 +20,45 @@ for door in DOORS:
 
 picked_door = input("Pick your door: ")
 # picked_door = "2"
+show_door = ""
 
 if DOORS[picked_door] == "gold":  # Will always show 1st non-picked door
-    for door in DOORS:
+    for door, prize in DOORS.items():
         if picked_door == door:
             continue
+        show_door = door
         print(f"Your door: {picked_door}, door {door}: {DOORS[door]}")
         break
 elif DOORS[picked_door] == "goat":
-    for door in DOORS:
-        if picked_door == door or DOORS[door] == "gold":
+    for door, prize in DOORS.items():
+        if picked_door == door or prize == "gold":
             continue
+        show_door = door
         print(f"Your door: {picked_door}, door {door}: {DOORS[door]}")
 elif DOORS[picked_door] == "fleas":
-    for door in DOORS:
-        if picked_door == door or DOORS[door] == "gold":
+    for door, prize in DOORS.items():
+        if picked_door == door or prize == "gold":
             continue
+        show_door = door
         print(f"Your door: {picked_door}, door {door}: {DOORS[door]}")
+
+switch = input("Do you want to swith? [y/n]")
+if switch == "y":
+    if picked_door == "1":
+        if show_door == "2":
+            picked_door = "3"
+        elif show_door == "3":
+            picked_doo = "2"
+    elif picked_door == "2":
+        if show_door == "1":
+            picked_door = "3"
+        elif show_door == "3":
+            picked_door = "1"
+    elif picked_door == "3":
+        if show_door == "1":
+            picked_door = "2"
+        elif show_door == "2":
+            picked_door = "1"
+
+print(picked_door)
+print(DOORS[picked_door])
