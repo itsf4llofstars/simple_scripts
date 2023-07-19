6297f5472cef91ac06b9d8733fb86580c7f9fccf
itsf4llofstars <90528743+itsf4llofstars@users.noreply.github.com>
2023-07-18 19:20:47 -0500

    Add monty_hall.py
    A monty hall like game

---
+++ monty_hall.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import random
+import sys
+
+doors = ["one", "two", "three"]
+prizes = ["goat", "gold", "goat"]
+
+door_dict = {
+    "1": "",
+    "2": "",
+    "3": "",
+}
+
+win, lose = 0, 0
+
+for _ in range(len(prizes) ** len(prizes)):
+    random.shuffle(prizes)
+
+index = 0
+for k in door_dict.keys():
+    door_dict[k] = prizes[index]
+    index += 1
+del index
+
+for k, v in door_dict.items():
+    print(k, v)
+print()
+
+door_choice = input("Enter your door choice 1, 2, 3: ")
+
+show_door = ""
+for door in door_dict.keys():
+    if door == door_choice or door_dict[door] == 'gold':
+        continue
+    show_door = door
+    print(f"Door: {show_door} is a {door_dict[door]}")
+
+swap: bool = None
+
+switch = input("Do you want to switch [y/n]? ")
+
+if switch == 'y':
+    swap = True
+else:
+    swap = False
+
+if switch:
+    for door in door_dict.keys():
+        if door_dict[door] == 'gold':
+            win += 1
+        else:
+            lose += 1
+else:
+    if door_dict[door_choice] == 'gold':
+        win += 1
+    else:
+        lose += 1
+
+print()
+print(door_choice)
+for k, v in door_dict.items():
+    print(k, v)
+print(f"{win = } {lose = }")
